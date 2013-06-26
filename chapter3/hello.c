f36bd147f3d8ff5e6c61147afd69be381cad8641
darrel <tiandewei07@163.com>
2013-06-26 15:49:36 +0800

    chapter3 exercise

---
+++ chapter3/hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: wolf
+    > Mail: tiandewei07@163.com 
+    > Created Time: Wed 26 Jun 2013 03:44:29 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("hello world!\n");
+
+	return 0;
+}
